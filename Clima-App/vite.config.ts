0236cdbc2b1c203c3a410ea2505983f5631533ca
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2025-09-29 02:17:01 -0300

    Alterando o vite.config para resolver bug no deploy

--- Clima-App/vite.config.ts
+++ Clima-App/vite.config.ts
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react( )],
+  define: {
+    // Garante que a variável de ambiente VITE_OPENWEATHER_API_KEY seja injetada no bundle
+    'import.meta.env.VITE_OPENWEATHER_API_KEY': JSON.stringify(process.env.VITE_OPENWEATHER_API_KEY),
+  },
 })
